0ff96a04e4a3544f0ff70bf9e94e00281e5f5505
noelchew <chewwengchuen@gmail.com>
2021-02-27 23:16:21 +0800

    Removed Wildcard Import

--- app/src/main/java/com/example/androiddevchallenge/puppy/PuppyUi.kt
+++ app/src/main/java/com/example/androiddevchallenge/puppy/PuppyUi.kt
@@ -7,7 +7,14 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
